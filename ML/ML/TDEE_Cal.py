1c332e1683528bbb7931cadd79150ddaf20f6b30
WAG395 <158130936+WAG395@users.noreply.github.com>
2024-02-25 09:12:26 +0530

    Added TDEE Calculation Part

--- ML/ML/TDEE_Cal.py
+++ ML/ML/TDEE_Cal.py
@@ -1,13 +1,13 @@
-def calculate_bmr(age, weight, height, gender):
+def calculate_bmr(age, weight, height, gender): #Calculate BMR Function
     if gender.lower() == 'male':
-        bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age)
+        bmr = 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age) # Calculate male BMR Value
     elif gender.lower() == 'female':
-        bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age)
+        bmr = 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age) # Calculate female BMR Value
     else:
-        raise ValueError("Invalid gender! Please enter 'male' or 'female'.")
+        raise ValueError("Invalid gender! Please Enter 'male' or 'female'.") #Display a error message
     return bmr
 
-def calculate_tdee(bmr, activity_level):
+def calculate_tdee(bmr, activity_level): # calculate TDEE Function
     activity_multipliers = {
         'sedentary': 1.2,
         'lightly active': 1.375,
@@ -20,7 +20,7 @@ def calculate_tdee(bmr, activity_level):
     else:
         raise ValueError("Invalid activity level! Please enter sedentary, lightly active, moderately active, very active or extra active correctly.")
 
-def main():
+def main(): #main function
     # Get user inputs
     age = int(input("Enter your age (in years): "))
     weight = float(input("Enter your weight (in kilograms): "))
@@ -29,14 +29,14 @@ def main():
     activity_level = input("Enter your activity level "
                            "(sedentary, lightly active, moderately active, very active, extra active): ")
 
-    # Calculate BMR
+    # Calculate BMR value
     bmr = calculate_bmr(age, weight, height, gender)
 
-    # Calculate TDEE
+    # Calculate TDEE value
     tdee = calculate_tdee(bmr, activity_level)
 
     # Display the result TDEE Value
     print("Your Total Daily Energy Expenditure (TDEE) is:", tdee)
 
 if __name__ == "__main__":
-    main()
+    main()  #main function
