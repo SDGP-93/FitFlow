223fb98cdb9bdb2e3b355db86ddffca195e11ce2
FalihAhamed <152671645+FalihAhamed@users.noreply.github.com>
2024-03-04 08:50:56 +0530

    BMR and TDEE calculator function added

--- lib/Screens/input.dart
+++ lib/Screens/input.dart
@@ -34,7 +34,7 @@ class UserInputPage extends StatefulWidget {
 }
 
 class _UserInputPageState extends State<UserInputPage> {
-  String _selectedLevelOption = 'Beginner';
+  String _selectedLevelOption = 'sedentary';
   String _selectedGender = 'Male';
   TextEditingController _ageController = TextEditingController();
   TextEditingController _heightController = TextEditingController();
@@ -67,6 +67,32 @@ class _UserInputPageState extends State<UserInputPage> {
     });
   }
 
+  double calculate_bmr(int age, int weight, int height, String gender) {
+    if (gender.toLowerCase() == 'male') {
+      return 88.362 + (13.397 * weight) + (4.799 * height) - (5.677 * age);
+    } else if (gender.toLowerCase() == 'female') {
+      return 447.593 + (9.247 * weight) + (3.098 * height) - (4.330 * age);
+    } else {
+      throw ArgumentError("Invalid gender! Please Enter your gender as 'male' or 'female'.");
+    }
+  }
+
+  double calculate_tdee(double bmr, String activity_level) {
+    Map<String, double> activity_multipliers = {
+      'sedentary': 1.2,
+      'lightly active': 1.375,
+      'moderately active': 1.55,
+      'very active': 1.725,
+      'extra active': 1.9
+    };
+
+    if (activity_multipliers.containsKey(activity_level.toLowerCase())) {
+      return bmr * activity_multipliers[activity_level.toLowerCase()]!;
+    } else {
+      throw ArgumentError("Invalid activity level! Please enter correctly.");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -96,13 +122,6 @@ class _UserInputPageState extends State<UserInputPage> {
                   fillColor: Colors.white70,
                 ),
                 style: TextStyle(color: Colors.black),
-                validator: (value) {
-                  final age = int.tryParse(value ?? '');
-                  if (age == null || !(age >= 15 && age <= 60)) {
-                    return 'Please enter a valid age (15-60)';
-                  }
-                  return null;
-                },
               ),
               SizedBox(height: 20),
               TextFormField(
@@ -115,12 +134,6 @@ class _UserInputPageState extends State<UserInputPage> {
                   fillColor: Colors.white70,
                 ),
                 style: TextStyle(color: Colors.black),
-                validator: (value) {
-                  if (int.tryParse(value ?? '') == null) {
-                    return 'Please enter a valid weight';
-                  }
-                  return null;
-                },
               ),
               SizedBox(height: 20),
               TextFormField(
@@ -133,12 +146,6 @@ class _UserInputPageState extends State<UserInputPage> {
                   fillColor: Colors.white70,
                 ),
                 style: TextStyle(color: Colors.teal),
-                validator: (value) {
-                  if (int.tryParse(value ?? '') == null) {
-                    return 'Please enter a valid height';
-                  }
-                  return null;
-                },
               ),
               SizedBox(height: 60),
               Row(
@@ -184,14 +191,14 @@ class _UserInputPageState extends State<UserInputPage> {
                   GestureDetector(
                     onTap: () {
                       setState(() {
-                        _selectedLevelOption = 'Beginner';
+                        _selectedLevelOption = 'sedentary';
                       });
                     },
                     child: Container(
                       width: 70,
                       height: 70,
                       decoration: BoxDecoration(
-                        color: _selectedLevelOption == 'Beginner' ? Colors.teal : Colors.transparent,
+                        color: _selectedLevelOption == 'sedentary' ? Colors.teal : Colors.transparent,
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Image.asset('assets/begBtn.png'),
@@ -201,14 +208,14 @@ class _UserInputPageState extends State<UserInputPage> {
                   GestureDetector(
                     onTap: () {
                       setState(() {
-                        _selectedLevelOption = 'Mid';
+                        _selectedLevelOption = 'lightly active';
                       });
                     },
                     child: Container(
                       width: 70,
                       height: 70,
                       decoration: BoxDecoration(
-                        color: _selectedLevelOption == 'Mid' ? Colors.teal : Colors.transparent,
+                        color: _selectedLevelOption == 'lightly active' ? Colors.teal : Colors.transparent,
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Image.asset('assets/midBtn.png'),
@@ -218,14 +225,14 @@ class _UserInputPageState extends State<UserInputPage> {
                   GestureDetector(
                     onTap: () {
                       setState(() {
-                        _selectedLevelOption = 'Pro';
+                        _selectedLevelOption = 'moderately active';
                       });
                     },
                     child: Container(
                       width: 70,
                       height: 70,
                       decoration: BoxDecoration(
-                        color: _selectedLevelOption == 'Pro' ? Colors.teal : Colors.transparent,
+                        color: _selectedLevelOption == 'moderately active' ? Colors.teal : Colors.transparent,
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Image.asset('assets/proBtn.png'),
@@ -234,14 +241,14 @@ class _UserInputPageState extends State<UserInputPage> {
                   GestureDetector(
                     onTap: () {
                       setState(() {
-                        _selectedLevelOption = 'liv';
+                        _selectedLevelOption = 'very active';
                       });
                     },
                     child: Container(
                       width: 70,
                       height: 70,
                       decoration: BoxDecoration(
-                        color: _selectedLevelOption == 'liv' ? Colors.teal : Colors.transparent,
+                        color: _selectedLevelOption == 'very active' ? Colors.teal : Colors.transparent,
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Image.asset('assets/midBtn.png'),
@@ -251,14 +258,14 @@ class _UserInputPageState extends State<UserInputPage> {
                   GestureDetector(
                     onTap: () {
                       setState(() {
-                        _selectedLevelOption = 'kro';
+                        _selectedLevelOption = 'extra active';
                       });
                     },
                     child: Container(
                       width: 70,
                       height: 70,
                       decoration: BoxDecoration(
-                        color: _selectedLevelOption == 'kro' ? Colors.teal : Colors.transparent,
+                        color: _selectedLevelOption == 'extra active' ? Colors.teal : Colors.transparent,
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Image.asset('assets/midBtn.png'),
@@ -267,81 +274,85 @@ class _UserInputPageState extends State<UserInputPage> {
                 ],
               ),
               SizedBox(height: 25),
-          Center(
-          child: Container(
-            width: 170,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30.0),
-              gradient: LinearGradient(
-                colors: [Colors.black, Colors.black],
-                begin: Alignment.centerLeft,
-                end: Alignment.centerRight,
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.teal,
-                  offset: Offset(0, 2),
-                  blurRadius: 3,
-                ),
-              ],
-            ),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.transparent,
-                  foregroundColor: Colors.transparent,
-                  elevation: 0,
-                  padding: EdgeInsets.symmetric(
-                    vertical: 15,
-                    horizontal: 30,
+              Center(
+                child: Container(
+                  width: 170,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    gradient: LinearGradient(
+                      colors: [Colors.black, Colors.black],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.teal,
+                        offset: Offset(0, 2),
+                        blurRadius: 3,
+                      ),
+                    ],
                   ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-                onPressed: _isButtonDisabled ? null : () async {
-                  // Prepare data to be stored in Firestore
-                  Map<String, dynamic> inputData = {
-                    'age': _ageController.text,
-                    'weight': _weightController.text,
-                    'height': _heightController.text,
-                    'gender': _selectedGender,
-                    'level': _selectedLevelOption,
-                  };
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: Colors.transparent,
+                      elevation: 0,
+                      padding: EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 30,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: _isButtonDisabled ? null : () async {
+                      final age = int.tryParse(_ageController.text) ?? 0;
+                      final weight = int.tryParse(_weightController.text) ?? 0;
+                      final height = int.tryParse(_heightController.text) ?? 0;
 
-                  // Get the current user
-                  final currentUser = FirebaseAuth.instance.currentUser;
-                  if (currentUser != null) {
-                    try {
-                      // Store data in Firestore under 'InputSubmit' collection
-                      await FirebaseFirestore.instance
-                          .collection('InputSubmit')
-                          .doc(currentUser.uid) // Use user's UID as document ID
-                          .set(inputData);
+                      final bmr = calculate_bmr(age, weight, height, _selectedGender);
+                      final tdee = calculate_tdee(bmr, _selectedLevelOption);
 
-                      // Navigate to MyWorkoutPlanPage
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => MyWorkoutPlanPage(),
-                        ),
-                      );
-                    } catch (error) {
-                      print('Error storing input data: $error');
-                      // Handle error as needed
-                    }
-                  }
-                },
-                child: Text('Submit',
-                  style: TextStyle(
-                      color: Colors.teal,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: ''
+                      final inputData = {
+                        'age': age.toString(),
+                        'weight': weight.toString(),
+                        'height': height.toString(),
+                        'gender': _selectedGender,
+                        'level': _selectedLevelOption,
+                        'bmr': bmr.toString(),
+                        'tdee': tdee.toString(),
+                      };
+
+                      final currentUser = FirebaseAuth.instance.currentUser;
+                      if (currentUser != null) {
+                        try {
+                          await FirebaseFirestore.instance
+                              .collection('InputSubmit')
+                              .doc(currentUser.uid)
+                              .set(inputData);
+
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MyWorkoutPlanPage(bmr, tdee),
+                            ),
+                          );
+                        } catch (error) {
+                          print('Error storing input data: $error');
+                        }
+                      }
+                    },
+                    child: Text('Submit',
+                      style: TextStyle(
+                          color: Colors.teal,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: ''
+                      ),
+                    ),
                   ),
                 ),
               ),
-              ),
-          ),
             ],
           ),
         ),
@@ -351,14 +362,119 @@ class _UserInputPageState extends State<UserInputPage> {
 }
 
 class MyWorkoutPlanPage extends StatelessWidget {
+  final double bmr;
+  final double tdee;
+
+  MyWorkoutPlanPage(this.bmr, this.tdee);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('My Workout Plan'),
-      ),
-      body: Center(
-        child: Text('This is your new workout plan.'),
+      extendBodyBehindAppBar: true,
+      appBar: CommonNavBar(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/bmrBg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.fromLTRB(10, 180, 10, 20),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Your BMR',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '${bmr.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 50,
+                      ),
+                    ),
+                    SizedBox(height: 170),
+                    Text(
+                      'Your TDEE',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      '${tdee.toStringAsFixed(2)}',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 50,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 120),
+              Center(
+                child: Container(
+                  width: 180,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    gradient: LinearGradient(
+                      colors: [Colors.black, Colors.black],
+                      begin: Alignment.centerLeft,
+                      end: Alignment.centerRight,
+                    ),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.teal,
+                        offset: Offset(0, 2),
+                        blurRadius: 3,
+                      ),
+                    ],
+                  ),
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: Colors.transparent,
+                      elevation: 0,
+                      padding: EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 30,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    onPressed: () {
+                      // Add your logic to generate workout plan
+                    },
+                    child: Text(
+                      'Generate Workout Plan',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 14,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
